e9bf53df92c227104da71024579032f061313665
Ferreirabento <jf6808932@gmai.com>
2025-05-02 10:46:31 -0300

    fazendo filtros separadamente para que cada parte dependa unicamente de si mesmo. ou seja, em caso de erro, um bloco vai ser afetado individualmente sem cessar os outros

--- projeto_teste.py
+++ projeto_teste.py
@@ -8,25 +8,56 @@ def primeiro_tempo(): #tranformar isso em uma função para poder fazer isso com
     macara = arr == "1° Tempo" # a condição
     linhas_com_1tempo = np.any(macara, axis=1) # onde está cada uma das linhas que tem essa condição no eixo 1
     linha_filtrada = arr[linhas_com_1tempo] # filtro já funcionando
-    print(linha_filtrada) # mostrar o que foi feito
+    return linha_filtrada # mostrar o que foi feito
 
 
 def segundo_tempo():
     arr = np.array(conectar_dados)
     mascara = arr == "2° Tempo"
     linhas_com_2tempo = np.any(mascara, axis=1)
-    filtro = arr[linhas_com_2tempo]
-    print(filtro)
+    filtro = np.array(arr[linhas_com_2tempo])
+    return filtro
 
 def integral():
     arr = np.array(conectar_dados)
     mascara = arr == "Integral"
     linhas = np.any(mascara, axis=1)
-    filtro = arr[linhas]
+    filtro = np.array(arr[linhas])
+    return filtro
+
+def gols(tempo_array):
+    array = np.array(tempo_array)
+    marcara_gols = array == "Gols"
+    busca = np.any(marcara_gols, axis=1)
+    filtro = array[busca]
+    return filtro
+
+def finalizacoes(tempo_array):
+    arr = np.array(tempo_array)
+    marcara = arr == "Finalizações"
+    busca = np.any(marcara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+def chute_ao_gol(tempo_array):
+    arr = np.array(tempo_array)
+    mascara = arr == "Chutes ao Gol"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+def escanteios(tempo_array):
+    arr = np.array(tempo_array)
+    mascara = arr == "Escanteios"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+def cartao_amarelo(tempo_array):
+    arr = np.array(tempo_array)
+    mascara = arr == "Cartões Amarelo"
+    busca = np.any(mascara)
+    filtro = arr[busca]
     print(filtro)
 
-primeiro_tempo() # chamar a função
-print("-=" * 60)
-segundo_tempo()
-print("-=" * 60)
-integral()
+cartao_amarelo(primeiro_tempo())
