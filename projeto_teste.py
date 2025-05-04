60dd66cd4ab7b8c82762dbf63f1d0717c4587cd3
Ferreirabento <jf6808932@gmai.com>
2025-05-04 00:11:59 -0300

    ainda ajustando algumas coisa mas quase terminando a parte de filtro

--- projeto_teste.py
+++ projeto_teste.py
@@ -58,6 +58,81 @@ def cartao_amarelo(tempo_array):
     mascara = arr == "Cartões Amarelo"
     busca = np.any(mascara, axis=1)
     filtro = arr[busca]
-    print(filtro)
+    return filtro
+
+#cada um deles é um tipo de media
+#convertidos
+def convertidos(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Convertidos  "
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+#convertidos - casa
+def convertidos_casa(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Convertidos - Casa "
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+#convertidos - fora
+def convertidos_fora(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Convertidos - Fora"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
 
 
+#sofridos
+def sofridos(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Sofridos"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+#sofridos - casa
+def sofridos_casa(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Sofridos - Casa"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+#sofridos - fora
+def sofridos_fora(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Sofridos - Fora"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+#totais
+def totais(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Totais"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+#totais - casa
+def totais_casa(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Totais - Casa"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+#totais - fora
+def totais_fora(categoria):
+    arr = np.array(categoria)
+    mascara = arr == " Totais - Fora"
+    busca = np.any(mascara, axis=1)
+    filtro = arr[busca]
+    return filtro
+
+# me a media de gols convertidos no primeiro tempo
+print(convertidos(gols(primeiro_tempo())))
